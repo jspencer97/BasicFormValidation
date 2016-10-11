e6edf11b3009ae8460477bcee0946011852558d8
Jack Spencer <jackspencer@wlan-t-195-202-dhcp.CSUChico.EDU>
2016-10-10 17:21:03 -0700

    added if and else

--- form-validation.js
+++ form-validation.js
@@ -3,12 +3,19 @@ function validate(){
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
 
+
+if(userEntered.length<6){
   //Show message that there is an error with the username...
   document.getElementById("usernameError").innerHTML="Bad username.";
   document.getElementById("usernameError").classList.remove("hidden-message");
   document.getElementById("usernameError").classList.add("shown-message");
   //Turn the username items red
   document.getElementById("usernameGroup").classList.add("has-error");
+}
+ else{
+   document.getElementById("usernameGroup").classList.add("has-success");
+
+ }
 
   //Show message that there is an error with the password...
   document.getElementById("passwordError").innerHTML="Bad password.";
